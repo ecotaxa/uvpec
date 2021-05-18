1b293af3bb7e11f9debe285b6c663d456a909659
fricour <florian.ricour@hotmail.fr>
2021-05-18 11:23:16 +0000

    rename UVP6foo into uvpec

--- UVP6foo/.ipynb_checkpoints/train-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import xgboost as xgb
-
-# train 'optimal' model
-def train(best_tree_number, dtrain, params):
-    
-    bst = xgb.train(params, dtrain, num_boost_round = best_tree_number)
-    
-    return(bst)
