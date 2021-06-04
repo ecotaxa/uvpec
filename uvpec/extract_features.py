52296a5122cb360a4a5ad0710c115fb4944d701d
fricour <florian.ricour@hotmail.fr>
2021-06-04 06:12:15 +0000

    add safeguard for max 40 classes

--- uvpec/extract_features.py
+++ uvpec/extract_features.py
@@ -13,8 +13,8 @@ def extract_features(path_to_subfolders):
     # Lists of all subfolders within the path_to_subfolders
     _, folderList, _ = next(os.walk(path_to_subfolders))
 
-    # saveguard : the number of image folders should be greater than 40 (technical limit, see with Marc Picheral/Fabio Dias/Camille Catanalo)
-    if len(folderList > 4):
+    # saveguard : the number of image folders should not be greater than 40 (technical limit, see with Marc Picheral/Fabio Dias/Camille Catanalo)
+    if len(folderList > 40):
         raise ValueError('Max number of classes is 40.')
 
     # Threshold value used to split image pixels into foreground (> threshold) and background (<= threshold) pixels.
