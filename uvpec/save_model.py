6220361f5cc10964d8c5b8f42db9a7775011b066
fricour <florian.ricour@hotmail.fr>
2021-12-13 16:03:15 +0000

    update taxo conf gile

--- uvpec/save_model.py
+++ uvpec/save_model.py
@@ -14,6 +14,6 @@ def save_model(xgb_model, output_dir, key, n_categories):
     xgb_model.feature_names = None # clean model feature names before exporting
     xgb_model.dump_model(os.path.join(output_dir,'Muvpec_'+key+'.txt'))
     
-    # add number of categories (i.e. taxa) in the model.txt file (i.e. Muvpec_key.txt)
-    with open(os.path.join(output_dir,'Muvpec_'+key+'.txt'), 'a') as f:
-         f.write('categories_number='+str(n_categories)+'\n')
+    ## add number of categories (i.e. taxa) in the model.txt file (i.e. Muvpec_key.txt)
+    #with open(os.path.join(output_dir,'Muvpec_'+key+'.txt'), 'a') as f:
+    #     f.write('categories_number='+str(n_categories)+'\n')
