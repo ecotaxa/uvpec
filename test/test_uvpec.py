ccee1f9b81953402fcf03a00f0b9aebd798152a6
fricour <florian.ricour@hotmail.fr>
2022-02-04 16:03:45 +0000

    update test

--- test/test_uvpec.py
+++ test/test_uvpec.py
@@ -1,33 +1,22 @@
 import os
+import glob
 
 # run package (so it means that the package should be installed before... not sure it is the right way to go ..)
 # check if running the config file works (should be improved because there are plenty of steps..)
 def test_package():
-	os.system('uvpec ./config.yaml')
-	assert(os.path.isdir('../test_output')) == True
+    os.system('uvpec ./config.yaml')
+    assert(os.path.isdir('../test_output')) == True
 
 # check if features file exists
 def test_feature():
-	assert(os.path.isfile('../test_output/features.feather')) == True
-
-cfg_params = '0.2_5_0.0_False_20'
+    assert(os.path.isfile('../test_output/features.feather')) == True
 
 # check if inflexion data exists
 def test_inflexion():
-	assert(os.path.isfile('../test_output/inflexion_point_'+cfg_params+'.feather')) == True
+    arr = glob.glob('../test_output/inflexion_point*.feather')
+    assert(len(arr)>0) == True
 
 # check if models are created
 def test_models():
-	assert(os.path.isfile('../test_output/xgboost_'+cfg_params+'.model')) == True
-	assert(os.path.isfile('../test_output/xgboost_'+cfg_params+'_model.txt')) == True
-
-
-
-
-
-
-
-
-
-
-
+    arr = glob.glob('../test_output/xgboost_*.model')
+    assert(len(arr)>0) == True
