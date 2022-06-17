ece68fb13f811a81978aa1f6a0be429234dcbe14
fricour <florian.ricour@hotmail.fr>
2022-06-17 14:58:46 +0000

    fix typo

--- uvpec/__main__.py
+++ uvpec/__main__.py
@@ -59,7 +59,7 @@ def main():
 
     if evaluate_only:
         print('no training, model evaluation only')
-        uvpec.evaluate_model(n_jobs, test_set, xgb_model, inflexion_filename = 'toto', False, output_dir, key)
+        uvpec.evaluate_model(n_jobs, test_set, xgb_model,'toto', False, output_dir, key) # toto because we don't use the inflexion file in the evaluation process only
         sys.exit(0) # evaluation only, we stop here
 
     # Generate unique key to have a unique identification (ID)
