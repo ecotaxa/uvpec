351dd570879e5e47ac4c35265356f5551387885b
fricour <florian.ricour@hotmail.fr>
2022-09-06 14:32:01 +0000

    add save binary model

--- uvpec/save_model.py
+++ uvpec/save_model.py
@@ -1,4 +1,5 @@
 import os
+from cython_uvp6 import py_convert_txt_model_to_binary_model
 
 def save_model(xgb_model, output_dir, key, n_categories):
 
@@ -13,7 +14,11 @@ def save_model(xgb_model, output_dir, key, n_categories):
     # dump model to a text file
     xgb_model.feature_names = None # clean model feature names before exporting
     xgb_model.dump_model(os.path.join(output_dir,'Muvpec_'+key+'.txt'))
-    
+
+    # convert txt model to binary model
+    txt_model_bytes = bytes(os.path.join(output_dir,'Muvpec_'+key+'.txt'), encoding ='utf-8') # need to convert string to bytes
+    py_convert_txt_model_to_binary_model(n_categories, txt_model_bytes)
+
     ## add number of categories (i.e. taxa) in the model.txt file (i.e. Muvpec_key.txt)
     #with open(os.path.join(output_dir,'Muvpec_'+key+'.txt'), 'a') as f:
     #     f.write('categories_number='+str(n_categories)+'\n')
