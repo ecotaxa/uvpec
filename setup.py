3247972c54db67a14923d5940fb531d749466b4d
fricour <florian.ricour@hotmail.fr>
2023-08-28 13:58:34 +0000

    >= instead of == for libraries in setup file

--- setup.py
+++ setup.py
@@ -31,14 +31,14 @@ setup(
             Extension("cython_uvp6", ["cython_uvp6.pyx"], language="c++")]),
         include_dirs=[numpy.get_include()], # had to add it because of an error on my local machine. See also here: https://stackoverflow.com/questions/14657375/cython-fatal-error-numpy-arrayobject-h-no-such-file-or-directory
         install_requires=[
-            'numpy==1.19.5', # check for sup sign
-            'pandas==1.2.1',
-            'PyYaml==5.3.1',
-            'scikit-image==0.18.1',
-            'xgboost==1.3.3',
-            'sklearn==0.0',
-            'pyarrow==3.0.0',
-            'key-generator==1.0.3',
+            'numpy>=1.19.5', # check for sup sign
+            'pandas>=1.2.1',
+            'PyYaml>=5.3.1',
+            'scikit-image>=0.18.1',
+            'xgboost>=1.3.3',
+            'sklearn>=0.0',
+            'pyarrow>=3.0.0',
+            'key-generator>=1.0.3',
             'pillow>=8.2.2'
         ]
 )
